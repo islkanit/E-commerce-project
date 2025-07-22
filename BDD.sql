f854ef282f8cd30b3e56e54b576a88db9403611c
islkanit <Koufi613@gmail.com>
2025-07-22 17:33:12 +0100

    yes
    the databse script

--- BDD.sql
+++ BDD.sql
@@ -3,3 +3,11 @@ create database Ecommerce;
 
 --to use the Ecommerce database--
 use Ecommerce;
+--to create the table named commandes--
+create table commandes (
+    id int primary key auto_increment,
+    client_id int,
+    date_commande datetime,
+    montant decimal(10, 2),
+    foreign key (client_id) references clients(id)
+);
